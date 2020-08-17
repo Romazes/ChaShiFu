9520a74da30cf52052e311b2ffaaa9e3af1ed4a2
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-17 14:12:10 +0300

    Moving files to make Area Work

--- TeaStore.UI/Areas/Catalog/Controllers/HomeController.cs
+++ TeaStore.UI/Areas/Catalog/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using TeaStore.UI.Models;
 
 namespace TeaStore.UI.Controllers
 {
+    [Area("Catalog")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
