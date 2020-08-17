5b17207c29f5145789f7f6a5ba36395c3f45e023
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-17 14:02:10 +0300

    Adding Areas to project.

--- TeaStore.UI/Startup.cs
+++ TeaStore.UI/Startup.cs
@@ -62,7 +62,7 @@ namespace TeaStore.UI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Catalog}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
