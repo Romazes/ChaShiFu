8b2d22b3f6d14faa7932be19e8d110c42a1508ab
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-17 13:29:32 +0300

    Добавьте файлы проекта.

---
+++ TeaStore.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeaStore.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
