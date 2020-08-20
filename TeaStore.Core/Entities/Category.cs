f780ea27c67b95c1c8e0d4b2779c2118042eaefc
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-20 20:53:18 +0300

    5.17. Delete Category View

--- TeaStore.Core/Entities/Category.cs
+++ TeaStore.Core/Entities/Category.cs
@@ -6,6 +6,7 @@ namespace TeaStore.Core.Entities
     public class Category : BaseEntity
     {
         [Required, StringLength(15, MinimumLength = 3)]
+        [Display(Name = "Название категории")]
         public string Name { get; set; }
     }
 }
