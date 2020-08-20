53ece0a9c9a843af3a4d493938e97c0fb23a60ea
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-20 20:22:24 +0300

    5.11. Table Button Partial View Added exception handling to Create method.

--- TeaStore.UI/Areas/Admin/Controllers/CategoryController.cs
+++ TeaStore.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TeaStore.Core.Entities;
 using TeaStore.Core.Interfaces;
 
@@ -30,12 +31,24 @@ namespace TeaStore.UI.Areas.Admin.Controllers
         //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name")]Category category)
+        public async Task<IActionResult> Create([Bind("Name")] Category category)
         {
-            if (ModelState.IsValid)
+            try
             {
-                await _categoryRepository.Add(category);
-                return RedirectToAction(nameof(Index));
+
+
+                if (ModelState.IsValid)
+                {
+                    await _categoryRepository.Add(category);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
             }
             return View(category);
         }
