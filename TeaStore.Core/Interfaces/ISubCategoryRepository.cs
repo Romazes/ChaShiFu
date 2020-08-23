56f2cbf32a0fda4eb9bc54afc62c29137819a607
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-23 18:46:25 +0300

    6.2. Designe Sub Category Repository pattern.

---
+++ TeaStore.Core/Interfaces/ISubCategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using TeaStore.Core.Entities;
+
+namespace TeaStore.Core.Interfaces
+{
+    public interface ISubCategoryRepository : IAsyncRepository<SubCategory>
+    {
+    }
+}
---
+++ TeaStore.Infrastructure/Data/SubCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using TeaStore.Core.Entities;
+using TeaStore.Core.Interfaces;
+
+namespace TeaStore.Infrastructure.Data
+{
+    public class SubCategoryRepository : EfRepository<SubCategory>, ISubCategoryRepository
+    {
+        public SubCategoryRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
