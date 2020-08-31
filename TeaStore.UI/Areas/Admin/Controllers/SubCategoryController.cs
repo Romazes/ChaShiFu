09c6fe4cc2d55e6a976072dd23fc3912ef1be98e
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-08-31 20:27:29 +0300

    6.13. SubCategory Edit Get Action

--- TeaStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
+++ TeaStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
@@ -86,5 +86,63 @@ namespace TeaStore.UI.Areas.Admin.Controllers
             var subCategories = _subCategoryRepository.GetAll().Result.Where(n => n.CategoryId == id);
             return Json(new SelectList(subCategories, "Id", "Name"));
         }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _subCategoryRepository.GetById(id ?? 0) ;
+
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _categoryRepository.GetAll(),
+                SubCategory = subCategory,
+                SubCategoryList = await _subCategoryRepository.GetAllListUniqueOrderBy()
+            };
+
+            return View(model);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _subCategoryRepository.GetAll().Result
+                    .Where(n => n.Name == model.SubCategory.Name && n.Category.Id == model.SubCategory.CategoryId);
+
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "Error : Sub Category exists under " + doesSubCategoryExists.First().Category.Name
+                        + " category. Please use another name.";
+                }
+                else
+                {
+                    var subCategoryFromDb = await _subCategoryRepository.GetById(model.SubCategory.Id);
+                    subCategoryFromDb.Name = model.SubCategory.Name;
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _categoryRepository.GetAll(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _subCategoryRepository.GetAllListUniqueOrderBy(),
+                StatusMessage = StatusMessage
+            };
+
+            return View(modelVM);
+        }
     }
 }
