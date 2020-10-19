0b730770bc4feda771c67c086ff3bdbe1361cdcc
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-10-19 15:49:22 +0300

    7.3. Add MenuItem Index Action

--- TeaStore.Infrastructure/Data/CatalogItemRepository.cs
+++ TeaStore.Infrastructure/Data/CatalogItemRepository.cs
@@ -1,4 +1,7 @@
-﻿using TeaStore.Core.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeaStore.Core.Entities;
 using TeaStore.Core.Interfaces;
 
 namespace TeaStore.Infrastructure.Data
@@ -6,5 +9,12 @@ namespace TeaStore.Infrastructure.Data
     public class CatalogItemRepository : EfRepository<CatalogItem>, ICatalogItemRepository
     {
         public CatalogItemRepository(ApplicationDbContext context) : base(context) { }
+
+        public override async Task<IEnumerable<CatalogItem>> GetAll()
+        {
+            return await Context.Set<CatalogItem>().Include(c => c.Category)
+                .Include(sc => sc.SubCategory).ToListAsync();
+
+        }
     }
 }
---
+++ TeaStore.UI/Areas/Admin/Controllers/CatalogItemController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using TeaStore.Core.Interfaces;
+
+namespace TeaStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CatalogItemController : Controller
+    {
+        private readonly ICatalogItemRepository _catalogItemRepository;
+        //For upload images to server.
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CatalogItemController(ICatalogItemRepository catalogItemRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _catalogItemRepository = catalogItemRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        //GET - INDEX
+        public async Task<IActionResult> Index()
+        {
+            return View(await _catalogItemRepository.GetAll());
+        }
+    }
+}
